cafc3b92d0337ea964d255c336e2ea376c0948e8
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-13 10:28:03 +0200

    Update setPath.bat

--- setPath.bat
+++ setPath.bat
@@ -1,3 +1,3 @@
-@cd C:\Users\User_Lab\Documents\GitHub\G-v2
-@set PATH=C:\Users\User_Lab\Documents\node-v20.15.0-win-x64;%PATH%
+@cd C:\Users\User-Lab\Documents\estateui
+@set PATH=C:\Users\User-Lab\Documents\node-v20.16.0-win-x64;%PATH%
 @cmd.exe /K

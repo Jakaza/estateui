387ab3bc2fdedeb065a1c07b8366a8240b17f610
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-13 13:18:56 +0200

    Responsive Design

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,14 +1,45 @@
+import { useState } from 'react'
 import './searchBar.scss'
 
 
+
+const types = ["buy", "rent"];
+
 function SearchBar() {
+
+  const [query , setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  })
+
+
+  const switchType = (value) =>{
+    setQuery(prev => ({...prev, type: value}));
+  }
+
+
+
+
   return (
-    <div>
-      <div className="type"></div>
+    <div className='searchBar'>
+      <div className="type">
+
+    {
+      types.map((type) => (
+        <button className={query.type === type ? "active" : ""} key={type} onClick={()=> switchType(type)}>{type}</button>
+      ))
+    }
+
+      </div>
       <form action="">
         <input type="text" name='location' placeholder='City Location ' />
-        <input type="number" name='minPrice' placeholder='Min Price' />
-        <input type="number" name='maxPrice' placeholder='Max Price ' />
+        <input type="number" name='minPrice' min={0} max={10000000}  placeholder='Min Price' />
+        <input type="number" name='maxPrice' min={0} max={10000000} placeholder='Max Price ' />
+      <button>
+        <img src="/search.png" alt="" />
+      </button>
       </form>
     </div>
   )

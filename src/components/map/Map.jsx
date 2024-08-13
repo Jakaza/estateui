d26d9b6aed86ac7139adc78e759f62a87f0e1dd1
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-13 16:23:34 +0200

    Map Creation

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -3,11 +3,12 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import React from "react";
 import "./map.scss"; // Ensure this file contains the necessary styles
 import "leaflet/dist/leaflet.css";
+import Pin from "./../pin/Pin";
 
 // Default position for the map
 const position = [-26.2041, 28.0473];
 
-function Map() {
+function Map({ items }) {
   return (
     <MapContainer
       center={position}
@@ -19,11 +20,9 @@ function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {items.map((item) => (
+        <Pin key={item.id} item={item} />
+      ))}
     </MapContainer>
   );
 }

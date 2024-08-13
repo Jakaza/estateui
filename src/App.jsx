5961c13b097f79a2b0cb5b0c7e1240e39150f1f1
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-13 13:49:55 +0200

    Config Routing
    Change imports path
    Fix Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import Navbar from './components/navbar/Navbar'
-import './layout.scss'
-import HomePage from './routes/homePage'
+import Navbar from "./components/navbar/Navbar";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./routes/homePage/homePage";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-      <Navbar/>
-      </div>
-     <div className="content">
-      <HomePage/>
-     </div>
-    </div>
-  )
+    // <div className="layout">
+    //   <div className="navbar">
+    //   <Navbar/>
+    //   </div>
+    //  <div className="content">
+    //   <HomePage/>
+    //  </div>
+    // </div>
+
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
